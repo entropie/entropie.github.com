5fb384df76831525089b42d9241413a1e092d77e
Michael Trommer <mictro@gmail.com>
2009-10-24 00:13:33 +0200

    getting feed title from rel attribute

--- script/site.js
+++ script/site.js
@@ -1,18 +1,18 @@
 var github_feed = "http://github.com/feeds/entropie/commits/entropie.github.com/master?" + Math.random(10000)*11;
 var lastfm_feed = "http://ws.audioscrobbler.com/1.0/user/mictro/recenttracks.rss?" + Math.random(10000)*11;
 
-function mk_feed(url, target, title){
+function mk_feed(url, target){
     $(target).gFeed( 
         { 
             url: url,
-            title: title
+            title: $(target).attr("rel")
         } 
     ); 
 }
 
 $(document).ready(function() { 
-    mk_feed(lastfm_feed, ".lastfm-feed", "last.fm");
-    mk_feed(github_feed, ".github-feed", "Recent Changes");
+    mk_feed(lastfm_feed, ".lastfm-feed");
+    mk_feed(github_feed, ".github-feed");
     
     $.get("data/test.markdown", function(data){
         $("#tmp").html(superTextile(data));
