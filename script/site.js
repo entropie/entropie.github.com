f80177297e850538a7626fcbb0ea19f3efca527c
Michael Trommer <mictro@gmail.com>
2009-10-23 23:19:35 +0200

    flickr stuff and layout changes

--- script/site.js
+++ script/site.js
@@ -1,13 +1,30 @@
 
 
 $(document).ready(function() { 
-    var app = "https://github.com/entropie.private.actor.atom?token=a8a5cdf7e5c336d47488cd22bffbcec4";
-    var app = "http://chneukirchen.org/trivium/index.atom";
-    var app = "http://github.com/feeds/entropie/commits/entropie.github.com/master";
+    var github_feed = "http://github.com/feeds/entropie/commits/entropie.github.com/master?" + Math.random(10000)*11;
+    var lastfm_feed = "http://ws.audioscrobbler.com/1.0/user/mictro/recenttracks.rss?" + Math.random(10000)*11;
     $('.github-feed').gFeed( 
         { 
-            url: app,
+            url: github_feed,
             title: "Recent changes.",
         } 
     ); 
+    $('.lastfm-feed').gFeed( 
+        { 
+            url: lastfm_feed,
+            title: "Recent changes.",
+        } 
+    ); 
+
+
+
+});
+jQuery(function(){
+    $("#flickr").flickr({
+        api_key: "10f2a12eaf44ae246dcdfd7fe49c5e8b",
+        type:    "search",
+        user_id: "84036884@N00",
+        per_page: 7,
+        thumb_size:     "s"
+    });
 });
